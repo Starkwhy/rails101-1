7ce195cb427f5b6c1ddffcfc25ab1d5ada2f9ec2
Starkw <474114116@qq.com>
2017-07-14 23:32:01 +0800

    imlement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :title, :description, presence: true
+
+  has_many :GroupRelationship
+  has_many :members, through: :group_relationship, source: :user
 end
